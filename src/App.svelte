1750e3253ceeb54d032fd52d549f208963321ddb
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2021-12-30 23:43:58 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<title>Vaskel</title>
+	<link rel='icon' type='image/png' href='/images/avatar_circle.png'>
+	<link rel="stylesheet" href="./style.css">
+</svelte:head>
+
+<div class="container">
+	<h1>e.</h1>
+</div>
+
+<style>
+
+	div.container {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		align-items: center;
+	}
+
+	div.container h1 {
+		text-align: left;
+		justify-content: center;
+	}
+</style>

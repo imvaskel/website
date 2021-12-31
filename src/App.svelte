bb40e5b76a7ac213d1004910d87b7774f991fd3c
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2021-12-31 00:14:20 -0500

    move style.css and get centering setup

--- src/App.svelte
+++ src/App.svelte
@@ -5,20 +5,33 @@
 </svelte:head>
 
 <div class="container">
-	<h1>e.</h1>
+	<header>
+		<img src="images/avatar_circle.png" alt="avatar">
+		<h2>Hi, I'm Vaskel</h2>
+		<p>
+			There isn't much interesting here, but you can find my social media <a href="/socials">here</a> or my about me
+			<a href="/about">here.</a>
+		</p>
+	</header>
 </div>
 
 <style>
-
 	div.container {
 		height: 100%;
 		width: 100%;
-		display: flex;
+		display: grid;
 		align-items: center;
-	}
-
-	div.container h1 {
-		text-align: left;
 		justify-content: center;
 	}
+
+	div.container img {
+		height: 312px;
+		width: 312px;
+		align-self: center;
+	}
+
+	div.container h2 {
+		text-align: center;
+	}
+
 </style>

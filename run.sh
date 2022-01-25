f994f1b86aa1a6bdbf000efbdbde5f52a8c99241
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-01-25 16:14:56 -0500

    fix shell script syntax

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 docker build -t website:latest
 
-docker run -d
-    \ --restart unless-stopped
-    \ -p 8934:3000
-    \ website:latest
+docker run -d \
+    --restart unless-stopped \
+    -p 8934:3000 \
+    website:latest \

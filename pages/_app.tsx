6def2107411bff03ddec0780a11473527396250e
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-02-13 21:58:06 -0500

    remove accidental navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Nav from '../lib/nav'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} > 
-    <Nav />
+  
   </Component>
 }
 

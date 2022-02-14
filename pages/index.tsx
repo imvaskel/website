0cb239bfe8a3a8edf5eb7aa433c00ca23be52f5f
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-02-13 21:51:44 -0500

    finish up design

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,45 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import Nav from '../lib/nav'
 import Footer from '../lib/footer'
+import { AVATAR_URL } from '../lib/constants'
+import Head from 'next/head'
+
+const Avatar = <img
+  style = {{
+    height: "256px",
+    width: "256px",
+    borderRadius: "50%",
+    marginTop: "32px"
+  }}
+  src={AVATAR_URL + ".png?size=1024"}
+  onMouseOver={e => e.currentTarget.src = AVATAR_URL + ".gif?size=1024"}
+  onMouseLeave={e => e.currentTarget.src = AVATAR_URL + ".png?size=1024"}>
+</img>
 
 const Home: NextPage = () => {
   return (
-    <div className="container">
+    <div className="root_container">
+      <Head>
+        <title>Home</title>
+      </Head>
+
       <Nav />
-      <h1>Welcome!</h1>
-      <p>
-        Hi, I'm Vaskel.
-        If you wanna learn about me for some reason, you can click{" "}
-          <Link href="/about">
-            <a style={{
-              color: "#E91E63"
-            }}>
-              here
-            </a>
-          </Link>.
-      </p>
+      <div className="container">
+        
+        {Avatar}
+        <h1>Welcome!</h1>
+        <p>
+          Hi, I'm Vaskel.
+          If you wanna learn about me for some reason, you can click{" "}
+            <Link href="/about">
+              <a style={{
+                color: "#E91E63"
+              }}>
+                here
+              </a>
+            </Link>.
+        </p>
+      </div>
       <Footer />
     </div>
   )

abc4bd2b7060bfa21c5d994e5ffce31970ae8006
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-02-13 21:51:44 -0500

    finish up design

--- lib/footer.tsx
+++ lib/footer.tsx
@@ -1,21 +1,23 @@
 import styles from "../styles/footer.module.css"
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai"
+import { SiMinutemailer } from "react-icons/si"
+import { FaDiscord } from "react-icons/fa"
 
 const socials = {
-    discord: "",
-    twitter: ""
-}
-
-const style = {
-    width: "64px",
-    height: "64px"
+    discord: "https://discord.com/users/447422100798570496",
+    twitter: "https://twitter.com/imvaskel",
+    github: "https://github.com/imvaskel",
+    email: "mailto:contact@vaskel.gay"
 }
 
 const Footer = () => {
 
     return (
         <div className={styles.footer_container}>
-            <a href={socials.discord}> <img src="discord.svg" className={styles.icon}/> </a>
-            <a href={socials.twitter}> <img src="twitter.svg" className={styles.icon}/> </a>
+            <a href={socials.discord}> <FaDiscord className={styles.icon}/> </a>
+            <a href={socials.twitter}> <AiOutlineTwitter className={styles.icon}/> </a>
+            <a href={socials.github}> <AiFillGithub className={styles.icon}/> </a>
+            <a href={socials.email}> <SiMinutemailer className={styles.icon}/> </a>
         </div>
     )
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,54 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import styled from "styled-components";
+import Footer from "../lib/footer";
 import Nav from "../lib/nav";
+
+const Header = styled.h1`
+    font-size: 2em;
+    padding-bottom: 2%
+`
+
+const Paragraph = styled.p`
+    font-weight: 400;
+    padding-bottom: 2%;
+    padding-left: 0.5%;
+`
+
+const LI = styled.li`
+    padding-bottom: 2%;
+    font-weight: 400;
+`
 
 const About: NextPage = () => {
     return (
-        <div>
-            
+        <div className="root_container" style={{fontSize: "large"}}>
+            <Head>
+                <title>About</title>
+            </Head>
+            <Nav />
+
+            <Header>About Me</Header>
+            <Paragraph>
+                I'm not too sure what to put here, but here we go.
+                Hi, I'm Vaskel. I'm a bisexual high schooler from the US, I play video games and program. I know, so interesting.
+                The best place to contact me is on discord.
+                <br/>
+
+            </Paragraph>
+
+            <Header>Social Media</Header>
+            <Paragraph>
+                You can find my social media in the footer.
+            </Paragraph>
+
+            <Header>Languages</Header>
+            <Paragraph>These are my most proficient languages ranked from highest to lowest.</Paragraph>
+            <Paragraph>Python: This is my first language and my most liked one.</Paragraph>
+            <Paragraph>Kotlin: This is a language that I know decently well and like to program in if it isn't python.</Paragraph>
+            <Paragraph>Rust: I'm learning this one and enjoy it, though I wouldn't say I know it well.</Paragraph>
+            <Paragraph>C#/Java: I know both of these enough to manage, though I do not like them that much. </Paragraph>
+            <Footer />
         </div>
     )
 }

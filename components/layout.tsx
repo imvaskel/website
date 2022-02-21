2eba2d2cbb88a08d059c189147e452f83fc0fc9c
Jared <jaredlll08@gmail.com>
2022-02-21 05:13:45 +0200

    Fix uncentered content and make sure that the footer stays at the bottom

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,16 @@
+import styles from "./layout.module.css"
 import React, { Component, ReactElement } from "react";
 import Footer from "./footer";
 import Nav from "./nav";
 
 export default function Layout({children}: React.PropsWithChildren<{}>): ReactElement {
     return (
-        <>
+        <div className={styles.layout}>
             <Nav />
-            <main>
+            <main className={styles.main}>
                 { children }
             </main>
             <Footer />
-        </>
+        </div>
     )
 }

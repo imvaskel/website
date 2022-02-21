d13b6201f509f2466f374d15638c0271db045ed2
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-02-20 22:46:14 -0500

    add accessibility to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,10 +14,22 @@ const Footer = () => {
 
     return (
         <div className={styles.footer_container}>
-            <a href={socials.discord}> <FaDiscord className={styles.icon}/> </a>
-            <a href={socials.twitter}> <AiOutlineTwitter className={styles.icon}/> </a>
-            <a href={socials.github}> <AiFillGithub className={styles.icon}/> </a>
-            <a href={socials.email}> <SiMinutemailer className={styles.icon}/> </a>
+            <a href={socials.discord}> 
+                <span className={styles.sr_only}>Discord</span> 
+                <FaDiscord className={styles.icon}/> 
+            </a>
+            <a href={socials.twitter}> 
+                <span className={styles.sr_only}>Twitter</span>
+                <AiOutlineTwitter className={styles.icon}/> 
+            </a>
+            <a href={socials.github}> 
+                <span className={styles.sr_only}>GitHub</span>
+                <AiFillGithub className={styles.icon}/> 
+            </a>
+            <a href={socials.email}> 
+                <span className={styles.sr_only}>Email</span>
+                <SiMinutemailer className={styles.icon}/> 
+            </a>
         </div>
     )
 }

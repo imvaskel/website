8046337323ff6a1c001bea0dff83c5abda0191a4
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-02-28 14:57:01 -0500

    add 404 page styling

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Error from "next/error"
+import styles from "../styles/errors.module.css"
+
+export default function NotFound() {
+    return (
+        <div className={styles.error_404}>
+            <Error statusCode={404} />
+        </div>
+    )
+}

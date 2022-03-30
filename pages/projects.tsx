b6bd0c8aa0ed815930c299aab1b981ea60accd4e
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-03-30 13:37:10 -0400

    use client side rendering for repo cards

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,44 +1,25 @@
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { NextPage } from "next";
 import { Repository, Card } from "../components/card"
 import styles from "../styles/projects.module.css"
 import axios from "axios";
- 
-interface Props {
-    repos?: Array<Repository>;
-    error?: any;
-}
 
-const Projects : NextPage<Props> = ({repos, error}) => {
-    if (repos === undefined || error !== undefined) {
-        return (
-            <div>
-                <h1>Repositories failed to load. </h1>
-                <p>
-                    Details of the error, if applicable: {error.toString()}
-                </p>
-            </div>
-        )
-    }
+const Projects : NextPage = () => {
+    const [repos, setRepos]: [Repository[], (posts: Repository[]) => void] = useState([] as Array<Repository>)
+
+    useEffect(() => {
+        axios.get<Repository[]>("https://gh-pinned-repos.egoist.sh/?username=ImVaskel").then(res => {
+          setRepos(res.data);
+        }).catch(err => {
+          console.log(err.message);
+        });
+    }, [])
 
     return (
         <div className={styles.grid_container}>
             { repos.map(r => <Card repo={r} key={r.repo} />)}
         </div>
     )
-}
-
-Projects.getInitialProps = async ctx => {
-    try {
-        const res = await axios.get("https://gh-pinned-repos.egoist.sh/?username=ImVaskel")
-        const repos = res.data as Array<Repository>;
-        return {
-            repos
-        }
-    }
-    catch (error) {
-        return { error }
-    }
 }
 
 export default Projects

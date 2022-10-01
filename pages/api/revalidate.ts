9942a01483a46bf786f4eb2bd05d963d0d244de5
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-09-30 23:20:39 -0400

    fix type error

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import config from "../../config.json"
-// Revalidate
+
+// Allow me to make a request to have the website revalidate my avatar.
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.query.secret != config.token) {
         return res.status(401).json({message: "Invalid token"})

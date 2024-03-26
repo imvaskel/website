c1cd16f8d0ec684f6f3673644f1d853b81344480
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2024-03-26 13:26:27 -0400

    set socials to dynamic

--- app/api/socials/route.ts
+++ app/api/socials/route.ts
@@ -12,8 +12,11 @@ export const GET = async () => {
     );
   }
   catch {
+    console.warn("tried to get json data from user.json, but an error occurred.");
     socials = [];
   }
 
   return Response.json(socials.filter((s) => s.visibility === 1 && s.type != "domain"));
 }
+
+export const dynamic = "force-dynamic";

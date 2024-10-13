7935f846c6ee89ed58cfce6b3bc54ace0808c6f6
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2024-10-13 00:41:59 -0400

    change email

--- lib/socials.ts
+++ lib/socials.ts
@@ -2,6 +2,6 @@ export const socials = {
     discord: "https://discord.com/users/447422100798570496",
     twitter: "https://twitter.com/imvaskel",
     github: "https://github.com/imvaskel",
-    email: "mailto:contact@vaskel.gay",
+    email: "mailto:me@vaskel.gay",
     pronouns: "https://en.pronouns.page/@vaskel",
 };
